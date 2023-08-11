e781ea8078cc83a277087140c8c137b76573bb60
Michał Ziółkowski <mrmajki552@gmail.com>
2023-08-11 23:29:08 +0200

    feat: Add bootsplash screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,12 +3,19 @@ import { RootStackParamList } from './types';
 import { NavigationContainer } from '@react-navigation/native';
 import { ChatRoomScreen } from '../screens/ChatRoomScreen';
 import { RoomsScreen } from '../screens/RoomsScreen';
+import RNBootSplash from 'react-native-bootsplash';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Navigation = () => {
+    const hideSplashScreen = () => {
+        setTimeout(async () => {
+            await RNBootSplash.hide({ fade: true, duration: 500 });
+        }, 1000);
+    };
+
     return (
-        <NavigationContainer>
+        <NavigationContainer onReady={hideSplashScreen}>
             <Stack.Navigator initialRouteName="RoomsScreen" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="ChatRoomScreen" component={ChatRoomScreen} />
                 <Stack.Screen name="RoomsScreen" component={RoomsScreen} />
